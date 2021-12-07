d92c245a28c7ed09ae4fff806ddc62f0c87049dd
Oscar Szumiak <oscarszumiak@gmail.com>
2021-12-07 01:00:34 +0100

    Uzupełnienie kodu zad_29.c w katalogu Lab_7.

--- Laboratorium/Lab_7/zad_29.c
+++ Laboratorium/Lab_7/zad_29.c
@@ -1,11 +1,44 @@
 #include <stdio.h>
 
 /*
-/ Napisz program, ktory poprzez menu pozwala na zapisanie tekstu podanego
-/ przez uzytkownika do pliku albo wyswietlenie zawartosci
+/ Napisz program, ktory poprzez menu pozwala na zapisanie tekstu
+/ podanego przez uzytkownika do pliku albo wyswietlenie zawartosci
 / tego pliku tekstowego.
 */
 
 int main() {
+    FILE * plik;
+    char nazwa_pliku [64];
+    char c, menu;
+    do {
+        printf("Wybierz opcje:\n1 - Wyswietl zawartosc pliku\n2 - Zapisz tekst do pliku\n3 - Koniec\n");
+        scanf(" %c", &menu);
+        if (menu == '1') {
+            printf("Podaj nazwe pliku:");
+            scanf(" %[^\n]s", nazwa_pliku);
+            plik = fopen(nazwa_pliku, "r");
+            if (plik) {
+                while(1) {
+                    c = fgetc(plik);
+                    if (c != EOF) putchar(c);
+                    else break;
+                }
+            fclose(plik);
+            } else perror("Problem z otwarciem pliku");
+        } else if (menu == '2') {
+            printf("Podaj nazwe pliku:");
+            scanf(" %[^\n]s", nazwa_pliku);
+            plik = fopen(nazwa_pliku, "w");
+            if (plik) {
+                printf("Podaj tekst do zapisania:");
+                while(1) {
+                    c = getchar();
+                    if (c != '\n') fputc(c, plik);
+                    else break;
+                }
+            fclose(plik);
+            } else perror("Problem z otwarciem pliku");
+        } else printf("Nieprawidlowy wybor");
+    } while(menu != '3');
     return 0;
 }
